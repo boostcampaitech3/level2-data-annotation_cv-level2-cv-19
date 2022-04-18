f3e56e8060ec631a2bbbaa8cbba98f75cb8ed50d
Sangjin Lee <hn04147@naver.com>
2022-04-18 17:45:39 +0000

    code for downloading ICDAR19_MLT dataset

---
+++ code/download_ICDAR19_MLT.sh
@@ -0,0 +1,21 @@
+cd /opt/ml/input/data
+mkdir ICDAR19_MLT
+cd /opt/ml/input/data/ICDAR19_MLT
+mkdir images
+mkdir ufo
+
+cd /opt/ml/code
+
+wget https://datasets.cvc.uab.es/rrc/ImagesPart1.zip --no-check-certificate
+wget https://datasets.cvc.uab.es/rrc/ImagesPart2.zip --no-check-certificate
+
+for i in *.zip
+do
+    unzip $i -d /opt/ml/input/data/ICDAR19_MLT/images
+done
+
+wget https://datasets.cvc.uab.es/rrc/train_gt_t13.zip --no-check-certificate
+unzip train_gt_t13.zip -d /opt/ml/input/data/ICDAR19_MLT/ufo
+
+mv /opt/ml/input/data/ICDAR19_MLT/images/ImagesPart1/* /opt/ml/input/data/ICDAR19_MLT/images
+mv /opt/ml/input/data/ICDAR19_MLT/images/ImagesPart2/* /opt/ml/input/data/ICDAR19_MLT/images
